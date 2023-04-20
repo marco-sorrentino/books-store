6fad029728815b7203a159f41d45d13fda110d96
Marco-Sorrentino <sorrentmarco@gmail.com>
2023-04-20 10:40:37 +0200

    installed redux

---
+++ src/redux/reducer/index.tsx
@@ -0,0 +1,12 @@
+const initialState = {
+  books: [],
+};
+
+const mainReducer = (state = initialState, action: any) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default mainReducer;
---
+++ src/redux/store/store.tsx
@@ -0,0 +1,6 @@
+import { configureStore } from "@reduxjs/toolkit";
+import mainReducer from "../reducer";
+
+export const store = configureStore({
+  reducer: mainReducer,
+});
